62bee8889d954245be0703e2149e9aa285c1dbfd
Matejko <matejjslavik@gmail.com>
2024-02-01 20:00:11 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+zoznam = ["b", "b", "c", "a", "b", "a"]
 def most_frequent(data: list[str]) -> str:
-    # your code here
-    return ""
-
-
-print("Example:")
-print(most_frequent(["a", "b", "c", "a", "b", "a"]))
-
-# These "asserts" are used for self-checking
-assert most_frequent(["a", "b", "c", "a", "b", "a"]) == "a"
-assert most_frequent(["a", "a", "bi", "bi", "bi"]) == "bi"
-
-print("The mission is done! Click 'Check Solution' to earn rewards!")
+    numero = 0
+    najcastejsie = ""
+    for i in zoznam:
+        if zoznam.count(i) > numero:
+            numero = zoznam.count(i)
+            najcastejsie = i
+    return najcastejsie
+print(most_frequent(zoznam))
